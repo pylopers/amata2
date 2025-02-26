ae78a354920db74d371a511c51958b632f776264
Abdul Kavi <pylopers@gmail.com>
2025-02-26 23:38:40 +0530

    mv

--- frontend/src/components/BestSeller.jsx
+++ frontend/src/components/BestSeller.jsx
@@ -25,9 +25,9 @@ const BestSeller = () => {
             <div className="md:grid md:grid-cols-4 lg:grid-cols-5 gap-4 flex space-x-4 overflow-x-auto scrollbar-hide px-4">
                 {bestSeller.map((item, index) => (
                     <div 
-                        key={index} 
-                        className="w-[90vw] h-[90vw] max-w-[250px] max-h-[450px] md:w-full md:h-auto flex-shrink-0"
-                    >
+                    key={index} 
+                    className="w-[90vw] h-auto min-h-[400px] max-w-[250px] max-h-[450px] md:w-full md:h-auto flex-shrink-0"
+                >
                         <ProductItem 
                             id={item._id} 
                             name={item.name} 
