ac5a5fd1ec147d4ca692e25c593cb7be706f3242
Abdul Kavi <pylopers@gmail.com>
2025-02-27 00:38:57 +0530

    slider

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -5,6 +5,11 @@ import { assets } from '../assets/assets';
 import RelatedProducts from '../components/RelatedProducts';
 import axios from 'axios';
 import { FaCheckCircle } from 'react-icons/fa';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import { Navigation, Pagination } from 'swiper/modules';
 
 const Product = () => {
   const navigate = useNavigate();
@@ -91,18 +96,20 @@ const Product = () => {
       {/*----------- Product Data-------------- */}
       <div className='flex gap-12 sm:gap-12 flex-col sm:flex-row'>
 
-        {/*---------- Product Images------------- */}
-        <div className='flex-1 flex flex-col-reverse gap-3 sm:flex-row'>
-          <div className='flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full'>
-              {
-                productData.image.map((item,index)=>(
-                  <img onClick={()=>setImage(item)} src={item} key={index} className='w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer' alt="" />
-                ))
-              }
-          </div>
-          <div className='w-full sm:w-[80%]'>
-              <img className='w-full h-auto' src={image} alt="" />
-          </div>
+         {/* Product Image Slider */}
+         <div className='flex-1'>
+          <Swiper
+            modules={[Navigation, Pagination]}
+            navigation
+            pagination={{ clickable: true }}
+            className="w-full h-auto"
+          >
+            {productData.image.map((item, index) => (
+              <SwiperSlide key={index}>
+                <img className="w-full h-auto" src={item} alt={`Slide ${index}`} />
+              </SwiperSlide>
+            ))}
+          </Swiper>
         </div>
 
         {/* -------- Product Info ---------- */}
