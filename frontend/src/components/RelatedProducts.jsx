c4e000b262a5be0cfbaf78ccf698d2e7f5314d7c
Abdul Kavi <pylopers@gmail.com>
2025-02-27 01:13:46 +0530

    relaterd

--- frontend/src/components/RelatedProducts.jsx
+++ frontend/src/components/RelatedProducts.jsx
@@ -3,6 +3,8 @@ import { ShopContext } from '../context/ShopContext'
 import Title from './Title';
 import ProductItem from './ProductItem';
 
+
+
 const RelatedProducts = ({category,subCategory}) => {
 
     const { products } = useContext(ShopContext);
@@ -23,18 +25,32 @@ const RelatedProducts = ({category,subCategory}) => {
     },[products])
 
   return (
-    <div className='my-2'>
-      <div className=' text-center text-xl py-2'>
-        <Title text1={'RELATED'} text2={"PRODUCTS"} />
-      </div>
+    <div className='mt-4'>
+            <div className='text-center text-3xl'>
+                <Title text1={'Related'} text2={'Products'} />
+            </div>
 
-      <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
-        {related.map((item,index)=>(
-            <ProductItem key={index} id={item._id} name={item.name} price={item.price} image={item.image}/>
-        ))}
-      </div>
-    </div>
-  )
+            {/* Scrollable on Mobile, Grid on Larger Screens */}
+            <div className="md:grid md:grid-cols-4 lg:grid-cols-5 gap-4 flex space-x-4 overflow-x-auto scrollbar-hide px-4">
+                {related.map((item, index) => (
+                    <div 
+                    key={index} 
+                    className="w-[90vw] h-auto min-h-[400px] max-w-[250px] max-h-[450px] md:w-full md:h-auto flex-shrink-0"
+                >
+                        <ProductItem 
+                            id={item._id} 
+                            name={item.name} 
+                            image={item.image} 
+                            price={item.price} 
+                            originalPrice={item.originalPrice}
+                            averageRating={item.averageRating}
+                            features={item.features} 
+                        />
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 }
 
 export default RelatedProducts
