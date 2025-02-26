1912d7b265a225aef618aba412c1839f74f8c9b6
Abdul Kavi <pylopers@gmail.com>
2025-02-26 23:56:44 +0530

    300px

--- frontend/src/components/RelatedProducts.jsx
+++ frontend/src/components/RelatedProducts.jsx
@@ -28,7 +28,7 @@ const RelatedProducts = ({category,subCategory}) => {
         <Title text1={'RELATED'} text2={"PRODUCTS"} />
       </div>
 
-      <div className='ml-8 w-[700px] h-[250px] grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
+      <div className='ml-8 w-[300px] h-[300px] grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
         {related.map((item,index)=>(
             <ProductItem key={index} id={item._id} name={item.name} price={item.price} image={item.image}/>
         ))}
