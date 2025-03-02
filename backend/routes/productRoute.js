fa0a0fbd3c84e08d7078a534841330ca20fddc11
Abdul Kavi <pylopers@gmail.com>
2025-03-02 20:13:02 +0530

    aao dekhe

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { listProducts, addProduct, removeProduct, singleProduct,  addProductReview, getProductReviews, updateProduct  } from '../controllers/productController.js'
+import { listProducts, addProduct, removeProduct, singleProduct,  addProductReview, getProductReviews, updateProduct,listAllProducts  } from '../controllers/productController.js'
 import upload from '../middleware/multer.js';
 import adminAuth from '../middleware/adminAuth.js';
 import authUser from '../middleware/auth.js'
@@ -9,6 +9,7 @@ const productRouter = express.Router();
 productRouter.post('/add',adminAuth,upload.fields([{name:'image1',maxCount:1},{name:'image2',maxCount:1},{name:'image3',maxCount:1},{name:'image4',maxCount:1},{name:'image5',maxCount:1},{name:'image6',maxCount:1},{name:'image7',maxCount:1},{name:'image8',maxCount:1},{name:'image9',maxCount:1},{name:'image10',maxCount:1}]),addProduct);
 productRouter.post('/remove',adminAuth,removeProduct);
 productRouter.post('/single',singleProduct);
+router.get("/all", listAllProducts);
 productRouter.get('/list',listProducts)
 
 productRouter.post('/update', adminAuth, updateProduct);
