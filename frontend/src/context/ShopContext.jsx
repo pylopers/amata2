dd00768ee9378da3e40df688a6a44a3a77fd54ad
Abdul Kavi <pylopers@gmail.com>
2025-03-02 15:59:09 +0530

    all

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -81,7 +81,7 @@ const ShopContextProvider = (props) => {
   const getProductsData = async () => {
     try {
       const response = await axios.get(backendUrl + "/api/product/list");
-      console.log("Products from DB:", products.length);
+      console.log("Products from DB:", allProducts.length);
       if (response.data.success) {
         const all = response.data.products.reverse();
         const main = all.filter((product) => product.mainProduct === true);
