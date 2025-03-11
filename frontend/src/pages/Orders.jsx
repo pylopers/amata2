eba964f72a688a97db0f755b4ef8b0c9d80530a3
Abdul Kavi <pylopers@gmail.com>
2025-03-11 15:13:34 +0530

    letsee

--- frontend/src/pages/Orders.jsx
+++ frontend/src/pages/Orders.jsx
@@ -49,7 +49,7 @@ const Orders = () => {
   }, [token]);
 
   return (
-    <div className='border-t pt-16'>
+    <div className='ml-4 border-t pt-16'>
       <div className='text-2xl'>
         <Title text1={'MY'} text2={'ORDERS'} />
       </div>
