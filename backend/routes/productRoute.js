7deb870960adfa22a1b29e24c92ae91707e9aef7
Abdul Kavi <pylopers@gmail.com>
2025-03-23 17:18:22 +0530

    admin

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -12,7 +12,19 @@ productRouter.post('/single',singleProduct);
 productRouter.get("/all", listAllProducts);
 productRouter.get('/list',listProducts)
 
-productRouter.post('/update', adminAuth, updateProduct);
+productRouter.post('/update', adminAuth, upload.fields([
+    { name: 'image1', maxCount: 1 },
+    { name: 'image2', maxCount: 1 },
+    { name: 'image3', maxCount: 1 },
+    { name: 'image4', maxCount: 1 },
+    { name: 'image5', maxCount: 1 },
+    { name: 'image6', maxCount: 1 },
+    { name: 'image7', maxCount: 1 },
+    { name: 'image8', maxCount: 1 },
+    { name: 'image9', maxCount: 1 },
+    { name: 'image10', maxCount: 1 }
+  ]), updateProduct);
+  
 
 // Product review routes
 productRouter.post('/addReview', authUser, addProductReview);
