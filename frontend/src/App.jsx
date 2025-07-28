c399d1c37793f49996fd9dc5da1d4b99a28261f5
Abdul Kavi <pylopers@gmail.com>
2025-07-28 16:36:43 +0530

    letscheck

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,12 +6,12 @@ import About from './pages/About'
 import Contact from './pages/Contact'
 import Product from './pages/Product'
 import Cart from './pages/Cart'
-//import Login from './pages/Login'
-import PlaceOrder from './pages/PlaceOrder'
+import Login from './pages/Login'
+//import PlaceOrder from './pages/PlaceOrder'
 //import Orders from './pages/Orders'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
-import SearchBar from './components/SearchBar'
+//import Navbar from './components/Navbar'
+//import Footer from './components/Footer'
+//import SearchBar from './components/SearchBar'
 //import TrackOrder from './pages/TrackOrder'
 //import DeliveryReturns from './pages/Delivery'
 import { ToastContainer } from 'react-toastify'
@@ -45,8 +45,9 @@ const App = () => {
           <Route path="/contact" element={<Contact />} />
           <Route path="/product/:productId" element={<Product />} />
           <Route path="/cart" element={<Cart />} />
-          <Route path="/place-order" element={<PlaceOrder />} />
-          {/*<Route path="/login" element={<Login />} />
+          
+          <Route path="/login" element={<Login />} />
+          {/*<Route path="/place-order" element={<PlaceOrder />} />
           <Route path="/orders" element={<Orders />} />
           <Route path="/verify" element={<Verify />} />
           <Route path="/track/:orderId" element={<TrackOrder />} />
