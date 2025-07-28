44f36bd9ca82c6444492b4d28cc0b2dd6621e6cf
Abdul Kavi <pylopers@gmail.com>
2025-07-28 16:53:28 +0530

    yekya bawasir hora hai

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ import About from './pages/About'
 import Contact from './pages/Contact'
 import Product from './pages/Product'
 import Cart from './pages/Cart'
-import Login from './pages/Login'
+//import Login from './pages/Login'
 import PlaceOrder from './pages/PlaceOrder'
 //import Orders from './pages/Orders'
 import Navbar from './components/Navbar'
@@ -39,16 +39,15 @@ const App = () => {
         <SearchBar />
         <Routes>
           <Route path="/" element={<Home />} />
-
           <Route path="/collection" element={<Collection />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/product/:productId" element={<Product />} />
           <Route path="/cart" element={<Cart />} />
           
-          <Route path="/login" element={<Login />} />
           <Route path="/place-order" element={<PlaceOrder />} />
-          {/*<Route path="/orders" element={<Orders />} />
+          {/*<Route path="/login" element={<Login />} />
+          <Route path="/orders" element={<Orders />} />
           <Route path="/verify" element={<Verify />} />
           <Route path="/track/:orderId" element={<TrackOrder />} />
           <Route path="/Delivery" element={<DeliveryReturns/>}/>
