61d2229c29ea1839598d141aaf3920a8e22a99e7
Abdul Kavi <pylopers@gmail.com>
2025-07-28 16:16:07 +0530

    debug3

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import { Routes, Route, Link } from 'react-router-dom'
 import Home from './pages/Home'
 import Collection from './pages/Collection'
-//import About from './pages/About'
-//import Contact from './pages/Contact'
+import About from './pages/About'
+import Contact from './pages/Contact'
 //import Product from './pages/Product'
 //import Cart from './pages/Cart'
 //import Login from './pages/Login'
@@ -41,9 +41,9 @@ const App = () => {
           <Route path="/" element={<Home />} />
 
           <Route path="/collection" element={<Collection />} />
-          {/*<Route path="/about" element={<About />} />
+          <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
-          <Route path="/product/:productId" element={<Product />} />
+          {/*<Route path="/product/:productId" element={<Product />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/login" element={<Login />} />
           <Route path="/place-order" element={<PlaceOrder />} />
