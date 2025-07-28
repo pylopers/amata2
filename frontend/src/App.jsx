3f399ed53591e37d8e7bf19359b6231bc66e414e
Abdul Kavi <pylopers@gmail.com>
2025-07-28 16:21:25 +0530

    debug4

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,8 +4,8 @@ import Home from './pages/Home'
 import Collection from './pages/Collection'
 import About from './pages/About'
 import Contact from './pages/Contact'
-//import Product from './pages/Product'
-//import Cart from './pages/Cart'
+import Product from './pages/Product'
+import Cart from './pages/Cart'
 //import Login from './pages/Login'
 //import PlaceOrder from './pages/PlaceOrder'
 //import Orders from './pages/Orders'
@@ -43,9 +43,9 @@ const App = () => {
           <Route path="/collection" element={<Collection />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
-          {/*<Route path="/product/:productId" element={<Product />} />
+          <Route path="/product/:productId" element={<Product />} />
           <Route path="/cart" element={<Cart />} />
-          <Route path="/login" element={<Login />} />
+          {/*<Route path="/login" element={<Login />} />
           <Route path="/place-order" element={<PlaceOrder />} />
           <Route path="/orders" element={<Orders />} />
           <Route path="/verify" element={<Verify />} />
