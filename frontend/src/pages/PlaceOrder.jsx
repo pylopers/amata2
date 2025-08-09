fde2ff1150323f57cce72faaffce548796c8ba56
pylopers <pylopers@gmail.com>
2025-08-09 16:06:24 +0530

    Update PlaceOrder.jsx

--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -254,7 +254,7 @@ const PlaceOrder = () => {
   const initPay = order => {
     if (!window.Razorpay) return toast.error("Razorpay SDK failed to load.");
     const options = {
-      key: import.meta.env.VITE_RAZORPAY_KEY_ID,
+      key: process.env.VITE_RAZORPAY_KEY_ID,
       amount: order.amount,
       currency: "INR",
       name: "AMATA",
